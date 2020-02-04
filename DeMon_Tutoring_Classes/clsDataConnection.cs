2496b32bada4b262fc05009452b4d49b392443d3
Thomas Barnes <tombarnes742@gmail.com>
2020-02-04 10:21:31 +0000

    Reverted changes by all team members

--- DeMon_Tutoring_Classes/clsDataConnection.cs
+++ DeMon_Tutoring_Classes/clsDataConnection.cs
@@ -10,11 +10,6 @@ namespace DeMon_Tutoring_Classes
 {
     class clsDataConnection
     {
-        //Testing branches
-        int branch = 0;
-        int branch2 = 15;
-        int branch3 = 0;
-        int var = 0;
         //connection object used to connect to the database
         SqlConnection connectionToDB = new SqlConnection();
         //data adapter used to transfer data to and from the database
