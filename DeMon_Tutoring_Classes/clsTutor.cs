58226b6b8d115de4f1f74ca1791250f72fbd35d5
Jakub Chamera <P2409490@my365.dmu.ac.uk>
2020-02-05 10:01:12 +0000

    Created a test and a new class for the tutor component

---
+++ DeMon_Tutoring_Classes/clsTutor.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DeMon_Tutoring_Classes
+{
+    class clsTutor
+    {
+    }
+}
---
+++ DeMon_Tutoring_Testing/UnitTest1.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace DeMon_Tutoring_Testing
+{
+    [TestClass]
+    public class tstTutor
+    {
+        [TestMethod]
+        public void ActiveTutorOk()
+        {
+            //create instance of the class we want to create
+            clsTutor aTutor = new clsTutor();
+   
+        }
+    }
+}
