7194be9632d8b18d033d84164c02c559c90d9bc8
Jakub Chamera <P2409490@my365.dmu.ac.uk>
2020-02-05 10:36:23 +0000

    Added rest of the tests for tutor class and populated tutor class with properties

--- DeMon_Tutoring_Classes/clsTutor.cs
+++ DeMon_Tutoring_Classes/clsTutor.cs
@@ -11,5 +11,12 @@ namespace DeMon_Tutoring_Classes
     {
         public bool Active { get; set; }
         public DateTime DateAdded { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string Email { get; set; }
+        public bool Availability { get; set; }
+        public string Subject { get; set; }
+        public int TotalNumberOfSubjects { get; set; }
+        public string Password { get; set; }
     }
 }
--- DeMon_Tutoring_Testing/tstTutor.cs
+++ DeMon_Tutoring_Testing/tstTutor.cs
@@ -31,17 +31,102 @@ namespace DeMon_Tutoring_Testing
         }
 
         [TestMethod]
-        public void DateAddedTutor()
+        public void ActiveFirstNameTutor()
         {
             //create instance of the class we want to create
             clsTutor aTutor = new clsTutor();
             //create some test data to assign to tutor
-            DateTime TestData = DateTime.Now.Date;
+            string TestData = "Bob";
             //assign the data to the tutor
-            aTutor.DateAdded = TestData;
+            aTutor.FirstName = TestData;
             //test to see that the two values are the same
-            Assert.AreEqual(aTutor.DateAdded, TestData);
+            Assert.AreEqual(aTutor.FirstName, TestData);
         }
+
+        [TestMethod]
+        public void ActiveLastNameTutor()
+        {
+            //create instance of the class we want to create
+            clsTutor aTutor = new clsTutor();
+            //create some test data to assign to tutor
+            string TestData = "Stevens";
+            //assign the data to the tutor
+            aTutor.LastName = TestData;
+            //test to see that the two values are the same
+            Assert.AreEqual(aTutor.LastName, TestData);
+        }
+
+        [TestMethod]
+        public void ActiveEmailTutor()
+        {
+            //create instance of the class we want to create
+            clsTutor aTutor = new clsTutor();
+            //create some test data to assign to tutor
+            string TestData = "bobstevens@123.kdas.com";
+            //assign the data to the tutor
+            aTutor.Email = TestData;
+            //test to see that the two values are the same
+            Assert.AreEqual(aTutor.Email, TestData);
+        }
+
+        [TestMethod]
+        public void ActiveAvailabilityTutor()
+        {
+            //create instance of the class we want to create
+            clsTutor aTutor = new clsTutor();
+            //create some test data to assign to tutor
+            Boolean TestData = true;
+            //assign the data to the tutor
+            aTutor.Availability = TestData;
+            //test to see that the two values are the same
+            Assert.AreEqual(aTutor.Availability, TestData);
+        }
+
+        [TestMethod]
+        public void ActiveSubjectTutor()
+        {
+            //create instance of the class we want to create
+            clsTutor aTutor = new clsTutor();
+            //create some test data to assign to tutor
+            string TestData = "Chemistry";
+            //assign the data to the tutor
+            aTutor.Subject = TestData;
+            //test to see that the two values are the same
+            Assert.AreEqual(aTutor.Subject, TestData);
+        }
+
+        [TestMethod]
+        public void ActiveTotalNumberOfSubjectsTutor()
+        {
+            //create instance of the class we want to create
+            clsTutor aTutor = new clsTutor();
+            //create some test data to assign to tutor
+            int TestData = 1;
+            //assign the data to the tutor
+            aTutor.TotalNumberOfSubjects = TestData;
+            //test to see that the two values are the same
+            Assert.AreEqual(aTutor.TotalNumberOfSubjects, TestData);
+        }
+
+        [TestMethod]
+        public void ActivePasswordTutor()
+        {
+            //create instance of the class we want to create
+            clsTutor aTutor = new clsTutor();
+            //create some test data to assign to tutor
+            string TestData = "abc123";
+            //assign the data to the tutor
+            aTutor.Password = TestData;
+            //test to see that the two values are the same
+            Assert.AreEqual(aTutor.Password, TestData);
+        }
+
+
+
+
+
+
+
 
 
     }
