41f6cddbdd0ee3aa402ab5621818002c874a633e
Tom Barnes <tombarnes742@gmail.com>
2020-02-10 15:44:31 +0000

    Implemented Staffing classes

---
+++ DeMon_Tutoring_Classes/Staffing_Classes/lib/Name.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DeMon_Tutoring_Classes.Staffing_Classes.lib
+{
+    class Name
+    {
+        //Fields
+        private string firstName { get; set; }
+        private string lastName { get; set; }
+
+        public Name()
+        {
+            firstName = " ";
+            lastName = " ";
+        }
+
+        public Name(string fN)
+        {
+            firstName = fN;
+            lastName = " ";
+        }
+
+        public Name(string fN, string lN)
+        {
+            firstName = fN;
+            lastName = lN;
+        }
+
+
+        //Methods
+
+        //Returns a name object as a string
+        public string getFullName()
+        {
+                return this.firstName + this.lastName;
+        }
+
+        /*Name seperator seperates a string of a full name into a first name and last name and then assigns those
+         * to a Name Object
+         */
+        public void nameSeperator(string fullName)
+        {
+            string[] name = fullName.Split(' ');
+            this.firstName = name[0];
+            this.lastName = name[1];
+        }
+
+
+    }
+}
---
+++ DeMon_Tutoring_Testing/Class Testing/Staffing_Tests/tstStaffing.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace DeMon_Tutoring_Testing.Class_Testing.Staffing_Tests
+{
+    [TestClass]
+    public class tstStaffing
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
