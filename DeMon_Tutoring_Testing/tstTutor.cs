2be86a17f8477ff8d9eec0307f0b517f965bcacf
Jakub Chamera <P2409490@my365.dmu.ac.uk>
2020-02-11 09:25:31 +0000

    added date added test to tutor testing and organised classes

--- DeMon_Tutoring_Testing/tstTutor.cs
+++ DeMon_Tutoring_Testing/tstTutor.cs
@@ -121,6 +121,20 @@ namespace DeMon_Tutoring_Testing
             Assert.AreEqual(aTutor.Password, TestData);
         }
 
+        [TestMethod]
+        public void DateAddedTutorOK()
+        {
+            //create an instance of the class we want to create
+            clsTutor aTutor = new clsTutor();
+            //create some test data to assign to the property
+            DateTime TestData = DateTime.Now.Date;
+            //assign the data to the property
+            aTutor.DateAdded = TestData;
+            //test to see that the two values are the same
+            Assert.AreEqual(aTutor.DateAdded, TestData);
+        }
+
+
 
 
 
