95b404e7edd2be8acb6b9c1a22f164aee6b1134c
Jordan Barwell <p2405537@my365.dmu.ac.uk>
2020-02-11 10:52:49 +0000

    created a customer test, and began class

---
+++ DeMon_Tutoring_Classes/clsCustomer.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DeMon_Tutoring_Classes
+{
+    public class clsCustomer
+    {
+        private bool Active { get; set; }
+
+        private int CustomerID { get; set; }
+
+        private bool Customer { get; set; }
+
+        private string FirstName { get; set; }
+
+        private string LastName { get; set; }
+
+
+
+    }
+}
