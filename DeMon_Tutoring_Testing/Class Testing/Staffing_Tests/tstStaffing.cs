f6403483a561164aeba023b3c1e63ee3045d8f69
Tom Barnes <tombarnes742@gmail.com>
2020-02-17 12:24:50 +0000

    Old changes

--- DeMon_Tutoring_Testing/Class Testing/Staffing_Tests/tstStaffing.cs
+++ DeMon_Tutoring_Testing/Class Testing/Staffing_Tests/tstStaffing.cs
@@ -1,6 +1,5 @@
 ﻿using System;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
-using static DeMon_Tutoring_Classes.Staffing_Classes.lib;
 
 namespace DeMon_Tutoring_Testing.Class_Testing.Staffing_Tests
 {
@@ -9,6 +8,8 @@ namespace DeMon_Tutoring_Testing.Class_Testing.Staffing_Tests
     {
         [TestMethod]
         public void TestMethod1()
-        {        }
+        {    
+           
+        }
     }
 }
