d22951756f790e832a55f9faef4a23a9bacf888b
Jordan Barwell <p2405537@my365.dmu.ac.uk>
2020-02-18 11:03:44 +0000

    class change

---
+++ DeMon_Tutoring_Classes/Customer_Classes/clsCustomer.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DeMon_Tutoring_Classes
+{
+    public class clsCustomer
+    {
+        private bool Active { get; set; }
+        private int CustomerID { get; set; }
+        private bool Customer { get; set; }
+        private string FirstName { get; set; }
+        private string LastName { get; set; }
+        private DateTime DateOfBirth { get; set; }
+        private string Email { get; set; }
+        private string PhoneNumber { get; set; }
+        private string Password { get; set; }
+        private string CardNo { get; set; }
+        private DateTime CardDate { get; set; }
+        private bool StudentStatus { get; set; }
+
+        public void clcCustomer(int cID, string fn, string ln, DateTime dob, string email, string number, string pword, string cardNo, DateTime cardDate, bool studentStat)
+        {
+            CustomerID = cID;
+            FirstName = fn;
+            LastName = ln;
+            DateOfBirth = dob;
+            Email = email;
+            PhoneNumber = number;
+            Password = pword;
+            CardNo = cardNo;
+            CardDate = cardDate;
+            StudentStatus = studentStat;
+
+        }
+
+    }
+}
